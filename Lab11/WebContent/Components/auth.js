64f9b67d6549bd98ce672f742a661868578e61da
thami <thami@DESKTOP-LQJ1807>
2020-05-11 00:18:07 +0530

    implement click event handler for the login button to Login

--- Lab11/WebContent/Components/auth.js
+++ Lab11/WebContent/Components/auth.js
@@ -1,3 +1,32 @@
-/**
- * 
- */
+$(document).on("click", "#btnLogin", function(event){
+	
+	// Clear Alerts /-------------------------------
+	$("#alertError").text("");
+	$("#alertError").hide();
+	
+	// Form Validation /----------------------------
+	var status = validateLoginForm();
+	if (status != true) {
+		
+		$("#alertError").text(status);
+		$("#alertError").show();
+		
+		return;
+		
+	}
+	
+	// If Valid /---------------------------------
+	$.ajax({
+		
+		url : "LoginAPI",
+		type : "POST",
+		data : $("#formLogin").serialize(),
+		dataType : "text",
+		complete : function(response, status){
+			
+			onLoginComplete(response.responseText, status);
+		}
+		
+	});
+	
+});
