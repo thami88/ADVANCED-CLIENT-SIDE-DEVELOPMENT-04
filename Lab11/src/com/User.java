d3f01dbc16bb1ac2626143ffd6e7903168121554
thami <thami@DESKTOP-LQJ1807>
2020-05-10 23:35:24 +0530

    Output text changed

--- Lab11/src/com/User.java
+++ Lab11/src/com/User.java
@@ -8,17 +8,17 @@ public class User {
 		
 		if (!un.equals("asd")) { // Hard coded test value
 			
-			output = "Invalid Username !";
+			output = "Invalid Username";
 			
 		} else if (un.equals("asd")) { // Hard Coded test value
 			
 			if (!pw.equals("asd")) { // Hard Coded test value
 				
-				output = "Invalid Password !";
+				output = "Invalid Password";
 				
 			} else if (pw.equals("asd")) { // Hard Coded Test Value
 				
-				output = "Success !";
+				output = "Success";
 
 			} // End else if
 
