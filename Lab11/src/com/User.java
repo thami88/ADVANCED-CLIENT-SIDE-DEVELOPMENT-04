b78179b0f45b2af05b742f3e4dd0eb29ec72ad7d
thami <thami@DESKTOP-LQJ1807>
2020-05-10 23:29:55 +0530

    Add code for authentication logic.

--- Lab11/src/com/User.java
+++ Lab11/src/com/User.java
@@ -1,5 +1,31 @@
 package com;
 
 public class User {
+	
+	public String login(String un, String pw) {
+		
+		String output = "";
+		
+		if (!un.equals("asd")) { // Hard coded test value
+			
+			output = "Invalid Username !";
+			
+		} else if (un.equals("asd")) { // Hard Coded test value
+			
+			if (!pw.equals("asd")) { // Hard Coded test value
+				
+				output = "Invalid Password !";
+				
+			} else if (pw.equals("asd")) { // Hard Coded Test Value
+				
+				output = "Success !";
+
+			} // End else if
+
+		}
+		
+		return output;
+		
+	}
 
 }
