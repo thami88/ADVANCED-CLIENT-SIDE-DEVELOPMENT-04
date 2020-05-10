73a2012b9a51c515ad2fab104c46ddfc92d6e4e7
thami <thami@DESKTOP-LQJ1807>
2020-05-11 00:31:04 +0530

    Redirect user to next view

--- Lab11/WebContent/Components/auth.js
+++ Lab11/WebContent/Components/auth.js
@@ -30,3 +30,37 @@ $(document).on("click", "#btnLogin", function(event){
 	});
 	
 });
+
+function onLoginComplete(response, status){
+	
+	if (status == "success") {
+		
+		var resultSet = JSON.parse(response);
+		
+		if (resultSet.status.trim() == "success") {
+			
+			document.location = "item.jsp";
+			
+		} else if (resultSet.status.trim() == "error") {
+			
+			$("#alertError").text(resultSet.data);
+			$("alertError").show();
+
+		}
+		
+	} else if (status == "error") {
+		
+		$("#alertError").text("Error While Login.");
+		$("#alertError").show();
+
+	} else {
+		
+		$("#alertError").text("Unknown Error While Login.");
+		$("#alertError").show();
+		
+	}
+	
+	$("#hidItemIDSave").val("");
+	$("#formItem")[0].rest();
+	
+}
